1f25306dcaf80d3fa9a882604fcad402473eab7f
wanjirungari2 <99500197+WanjiruNgari2@users.noreply.github.com>
2025-10-29 02:22:48 +0300

    Refactor string handling and welcome message logic
    unmute all working code

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
    //STRINGS
 // QUIZ:Create a var message that stores the str:"You have three new notifications"
-        // let message = "You have three new notifications"
-        // console.log(message);//You have three new notifications
-        // let username = "Drew"
-        // console.log(username);//Drew
-        // let wholeThing = message + ", " + username + "!"
-        // console.log(wholeThing);//You have three new notifications, Drew!
+        let message = "You have three new notifications"
+        console.log(message);//You have three new notifications
+        let username = "Drew"
+        console.log(username);//Drew
+        let wholeThing = message + ", " + username + "!"
+        console.log(wholeThing);//You have three new notifications, Drew!
 
 //QUIZ:create 2 vars name and greeting. the name var to tore your
 //name and greeting to store eg:"hi, my name is "
 //Create a 3rd var myGreeting that contatenates the 2 strings ang log myGreeting
-        // let name = "Dwight Schrute"
-        // let greeting = "hi, my name is "
-        // let myGreeting = greeting + name 
-        // console.log(myGreeting);//hi, my name is Dwight Schrute
+        let name = "Dwight Schrute"
+        let greeting = "hi, my name is "
+        let myGreeting = greeting + name 
+        console.log(myGreeting);//hi, my name is Dwight Schrute
 
-        // let points = 4
-        // let bonusPoints = "10"
-        // let totalPoints = points + bonusPoints // 4 + "10" = "410"
-        // console.log(totalPoints); //410 NOT 14!*NOTE!*
+        let points = 4
+        let bonusPoints = "10"
+        let totalPoints = points + bonusPoints // 4 + "10" = "410"
+        console.log(totalPoints); //410 NOT 14!*NOTE!*
 
-        // console.log(4 + 7);//11 
-        // console.log("2" + "4");//"24" all are strings here
-        // console.log("4" + 1);//"41" num is stringified
-        // console.log(100 + "100");//"100100" num is stringified
+        console.log(4 + 7);//11 
+        console.log("2" + "4");//"24" all are strings here
+        console.log("4" + 1);//"41" num is stringified
+        console.log(100 + "100");//"100100" num is stringified
 
 // grab the welcome-el paragraph and store it in a var welcomeEl
 let welcomeEl = document.getElementById("welcome-el")
@@ -38,4 +38,5 @@ welcomeEl.innerText =  greeting + namer
 
 //add an emoji to the end 👋
 //hint: count = count + 1
+
 welcomeEl.innerText += "👋" //OR welcomeEl.innerText = welcomeEl.innerText +  "👋" 
